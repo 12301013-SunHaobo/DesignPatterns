4aae9bd25f869941dc105a6c9cd21501fa3057dc
12301013-SunHaobo <12301013@bjtu.edu.cn>
2015-06-22 15:44:24 +0800

    01_SimpleFactory and 02_Strategy

---
+++ 01_SimpleFactory/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("计算A+-*/B");
+            Console.Write("输入数字A：");
+            string strNumberA = Console.ReadLine();
+            Console.Write("输入运算符：");
+            string strOperate = Console.ReadLine();
+            Console.Write("输入数字B：");
+            string strNumberB = Console.ReadLine();
+
+            Operation oper = OperationFactory.createOperation(strOperate);
+            oper.NumberA = Convert.ToDouble(strNumberA);
+            oper.NumberB = Convert.ToDouble(strNumberB);
+            Console.WriteLine(oper.GetResult());
+            Console.ReadLine();
+        }
+    }
+
+    public class Operation
+    {
+        private double _numberB = 0;
+        private double _numberA = 0;
+
+        public double NumberA
+        {
+            get { return _numberA; }
+            set { _numberA = value; }
+        }
+
+        public double NumberB
+        {
+            get { return _numberB; }
+            set { _numberB = value; }
+        }
+
+        public virtual double GetResult()
+        {
+            return 0;
+        }
+    }
+
+    class OperationAdd:Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA + NumberB;
+        }
+    }
+
+    class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA - NumberB;
+        }
+    }
+
+    class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA * NumberB;
+        }
+    }
+
+    class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            if (NumberB == 0)
+                throw new Exception("除数不能为0");
+            else
+                return NumberA / NumberB;
+        }
+    }
+
+    public class OperationFactory
+    {
+        public static Operation createOperation(string operate)
+        {
+            Operation oper = null;
+
+            switch(operate)
+            {
+                case "+":
+                    oper = new OperationAdd();
+                    break;
+                case "-":
+                    oper = new OperationSub();
+                    break;
+                case "*":
+                    oper = new OperationMul();
+                    break;
+                case "/":
+                    oper = new OperationDiv();
+                    break;
+                default:
+                    throw new Exception("不支持的运算");
+            }
+
+            return oper;
+        }
+    }
+}
---
+++ 02_Strategy/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Strategy
+{
+    public partial class Form1 : Form
+    {
+        double total = 0.0d;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbxType.Items.AddRange(new object[] { "正常收费", "打8折", "满300返100", });
+            cbxType.SelectedIndex = 0;
+            textPrice.Focus();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            CashContext csuper = new CashContext(cbxType.SelectedItem.ToString());
+            double price = Convert.ToDouble(textPrice.Text);
+            double num = Convert.ToDouble(textNum.Text);
+            double totalPrices = csuper.GetResult(price * num);
+
+            total += totalPrices;
+            lbxList.Items.Add("单价：" + textPrice.Text + "    数量：" + textNum.Text +
+                "   小计：" + totalPrices);
+            lblResult.Text = total.ToString();
+            textPrice.SelectAll();
+            textPrice.Focus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lbxList.Items.Clear();
+            cbxType.SelectedIndex = 0;
+            total = 0.0d;
+            lblResult.Text = total.ToString();
+            textPrice.Text = "";
+            textNum.Text = "";
+        }
+    }
+
+    //现金收费抽象类
+    abstract class CashSuper
+    {
+        public abstract double acceptCash(double money);
+    }
+
+    //正常收费子类
+    class CashNormal:CashSuper
+    {
+        public override double acceptCash(double money)
+        {
+            return money;
+        }
+    }
+
+    //打折收费子类
+    class CashRebate : CashSuper
+    {
+        private double moneyRebate = 1d;
+        public CashRebate(string mondyRebate)
+        {
+            this.moneyRebate = double.Parse(mondyRebate);
+        }
+
+        public override double acceptCash(double money)
+        {
+            return money * moneyRebate;
+        }
+    }
+
+    //返利收费子类
+    class CashReturn:CashSuper
+    {
+        private double moneyCondition = 0.0d;
+        private double moneyReturn = 0.0d;
+
+        public CashReturn(string moneyCondition, string moneyReturn)
+        {
+            this.moneyCondition = double.Parse(moneyCondition);
+            this.moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double acceptCash(double money)
+        {
+            double result = money;
+            if (money > moneyCondition)
+                result = money - Math.Floor(money / moneyCondition) * moneyReturn;
+            return result;
+        }
+    }
+
+    class CashContext
+    {
+        CashSuper cs = null;
+        public CashContext(string type)
+        {
+            switch(type)
+            {
+                case "正常收费":
+                    cs = new CashNormal();
+                    break;
+                case "满300返100":
+                    cs = new CashReturn("300", "100");
+                    break;
+                case "打8折":
+                    cs = new CashRebate("0.8");
+                    break;
+            }
+        }
+
+        public double GetResult(double money)
+        {
+            return cs.acceptCash(money);
+        }
+    }
+}
